320a1c7e29b9ece6f5e79d2f7968c68e35b34d57
meskee <matejskoro1@gmail.com>
2016-10-30 20:17:41 +0100

    Add files via upload

---
+++ dz2_zad7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Zad7
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var task = Task.Run(() => MainAsync());
+            Task.WaitAll(task);
+
+            Console.Read();
+        }
+
+        private static async void MainAsync()
+        {
+            Console.WriteLine(await FactorialDigitSum(1));
+            Console.WriteLine(await FactorialDigitSum(2));
+            Console.WriteLine(await FactorialDigitSum(4));
+            Console.WriteLine(await FactorialDigitSum(8));
+            Console.WriteLine(await FactorialDigitSum(16));
+        }
+
+        public static async Task<long> FactorialDigitSum(int num)
+        {
+            Task<int> task = Task.Run(() => (GetFactorial(num).ToString().Sum(c => c - '0')));
+            await task;
+            return task.Result;
+        }
+
+        static long GetFactorial(int num)
+        {
+            long result = num;
+            for (var i = 1; i < num; i++)
+                result = result * i;
+            return result;
+        }
+    }
+}
---
+++ dz2_zad8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz2_zad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main method is the only method that
+            // can’t be marked with async.
+            // What we are doing here is just a way for us to simulate
+            // async-friendly environment you usually have with
+            // other .NET application types (like web apps, win apps etc.) // Ignore main method, you can just focus on
+            //LetsSayUserClickedAButtonOnGuiMethod() as a
+            // first method in call hierarchy.
+            var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+            Console.Read();
+        }
+
+        private static void LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = GetTheMagicNumber();
+            Console.WriteLine(result);
+        }
+
+        private static long GetTheMagicNumber()
+        {
+            return IKnowIGuyWhoKnowsAGuy().Result;
+
+        }
+
+        private static async Task<long> IKnowIGuyWhoKnowsAGuy()
+        {
+            var t1 = IKnowWhoKnowsThis(10);
+            var t2 = IKnowWhoKnowsThis(5);
+            await Task.WhenAll(t1, t2);
+            return t1.Result + t2.Result;
+        }
+
+        private static async Task<long> IKnowWhoKnowsThis(int n)
+        {
+            return await dz2_zad7.Program.FactorialDigitSum(n);
+        }
+    }
+}
